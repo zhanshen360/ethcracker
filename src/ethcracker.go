8088ec163b349b278aa74bbc072dea6f24e43d5f
alexn <alexn@lexansoft.com>
2019-01-02 10:49:37 -0800

    2.16
    Fixed the compilation problem

--- src/ethcracker.go
+++ src/ethcracker.go
@@ -79,7 +79,7 @@ func main() {
     
     if *v > 0 {
         println( "------------------------------------------------")
-        println( "Ethereum Password Cracker v2.16")
+        println( "Ethereum Password Cracker v2.16 ")
         println( "Author: @AlexNa ")
         println( "------------------------------------------------")
         println( "Private Key File:", *pk )
